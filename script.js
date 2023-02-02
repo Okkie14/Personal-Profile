42abd230c78587c9249942a7343bc7508a634ffc
Okkert <okkertjoub14@gmail.com>
2023-02-03 00:32:11 +0200

    Dark mode function was created, and finished

---
+++ script.js
@@ -0,0 +1,23 @@
+let darkMode = localStorage.getItem('darkMode');
+const darkModeToggle = document.querySelector('#dark-mode-toggle');
+
+const enableDarkMode = () => {
+    document.body.classList.add('darkMode');
+    localStorage.setItem('darkMode', 'enabled');
+};
+
+const disableDarkMode = () => {
+    document.body.classList.remove('darkMode');
+    localStorage.setItem('darkMode', null);
+};
+
+darkModeToggle.addEventListener('click', () => {
+    darkMode = localStorage.getItem('darkMode');
+    if (darkMode !== 'enabled') {
+        console.log('enabled')
+        enableDarkMode();
+    } else {
+        disableDarkMode();
+        console.log('disabled');
+    }
+});
